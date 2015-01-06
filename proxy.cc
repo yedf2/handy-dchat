8e9f4577366907528d7a6587c9acd65d9940a121
yedongfu <yedongfu@yongche.com>
2015-01-06 11:38:05 +0800

    updated

--- proxy.cc
+++ proxy.cc
@@ -8,13 +8,24 @@ using namespace std;
 using namespace handy;
 
 long hashVersion;
-vector<TcpConnPtr> bucketCons;
+vector<string> bucketAddrs;
 map<string, TcpConnPtr> addrCons;
+map<long, TcpConnPtr> userConns;
 
 TcpConnPtr getConByUid(long uid) {
-    if (bucketCons.size() == 0) { return NULL; }
-    int bucket = uid % bucketCons.size();
-    return bucketCons[bucket];
+    if (bucketAddrs.size() == 0) { return NULL; }
+    int bucket = uid % bucketAddrs.size();
+    return addrCons[bucketAddrs[bucket]];
+}
+
+void versionMatched(TcpConnPtr con, string server) {
+    for (auto& uc: userConns) {
+        int bucket = uc.first % bucketAddrs.size();
+        if (bucketAddrs[bucket] == server) {
+            ChatMsg login(ChatMsg::Login, 0, uc.first, 0, "");
+            con->sendMsg(login.str());
+        }
+    }
 }
 
 zhandle_t * zk_init(Conf& conf, EventBase* base, new_hash_table_cb cb);
@@ -30,22 +41,72 @@ void notify_new_hash_table(EventBase* base, string hashtable, long version) {
     hashVersion = version;
 
     vector<Slice> lns = Slice(hashtable).split('\n');
-    exitif(bucketCons.size() && bucketCons.size() != lns.size(), "hash table size changed");
+    exitif(bucketAddrs.size() && bucketAddrs.size() != lns.size(), "hash table size changed");
 
-    bucketCons.clear();
-    vector<string> addrs;
+    bucketAddrs.clear();
     set<string> seta;
     map<string, TcpConnPtr> oldcons;
     swap(oldcons, addrCons);
     for (auto ln: lns) {
         zkNode nd;
         int r = nd.parse(ln);
-        exitif(r || nd.id != (int)addrs.size(), "bad hash table");
-        addrs.push_back(nd.server);
+        exitif(r || nd.id != (int)bucketAddrs.size(), "bad hash table");
+        bucketAddrs.push_back(nd.server);
         seta.insert(nd.server);
-        auto& con = addrCons[nd.server];
-        con = oldcons[nd.server];
-        bucketCons.push_back(con);
+        auto& con = addrCons[nd.server] = oldcons[nd.server];
+        if (con->context<long>() == hashVersion) {
+            versionMatched(con, nd.server);
+        }
+    }
+}
+
+struct RouterCtx {
+    long version;
+    string server;
+};
+
+void reconnectRouter(EventBase* base) {
+    for (auto& pr: addrCons) {
+        auto& con = pr.second;
+        if (!con) {
+            vector<Slice> vs = Slice(pr.first).split(':');
+            string host = vs[0];
+            short port = (short)util::atoi(vs[1].data());
+            con = TcpConn::createConnection(base, host, port, 3);
+            con->setCodec(new LineCodec);
+            string server(pr.first);
+            con->context<RouterCtx>().server = server;
+            con->onState([](TcpConnPtr con) {
+                if (con->getState() == TcpConn::Closed || con->getState() == TcpConn::Failed) {
+                    string& server = con->context<RouterCtx>().server;
+                    addrCons[server] = NULL;
+                }
+            });
+            con->onMsg([server](TcpConnPtr con, Slice msg) {
+                long& rversion = con->context<RouterCtx>().version;
+                if (msg.size() && msg[0] == '#') {
+                    long ver = util::atoi(msg.data()+1);
+                    exitif(rversion != 0 && ver != rversion+1, "version not continous");
+                    rversion = ver;
+                    if (rversion == hashVersion) {
+                        versionMatched(con, server);
+                    }
+                    return;
+                }
+                ChatMsg chat(msg);
+                if (chat.type == ChatMsg::Chat) {
+                    auto p = userConns.find(chat.toId);
+                    if (p == userConns.end()) {
+                        ChatMsg res(ChatMsg::Ack, chat.msgId, 0, chat.fromId, "user not found");
+                        con->sendMsg(res.str());
+                    } else {
+                        p->second->sendMsg(msg);
+                    }
+                } else {
+                    error("unexpected msg: %.*s", (int)msg.size(), msg.data());
+                }
+            });
+        }
     }
 }
 
@@ -54,7 +115,6 @@ int main(int argc, const char* argv[]) {
     int port = conf.getInteger("", "port", 0);
     exitif(port <= 0, "port should be positive interger");
 
-    map<long, TcpConnPtr> users; //生命周期比连接更长，必须放在Base前
     EventBase base;
 
     zhandle_t* zh = zk_init(conf, &base, notify_new_hash_table);
@@ -63,17 +123,16 @@ int main(int argc, const char* argv[]) {
     Signal::signal(SIGINT, [&]{ base.exit(); });
     Signal::signal(SIGTERM, [&]{ base.exit(); });
 
+    base.runAfter(0, [&base]{ reconnectRouter(&base); }, 3000);
     TcpServer chat(&base, "", port);
     chat.onConnCreate([&]{
         TcpConnPtr con(new TcpConn);
         con->setCodec(new LineCodec);
         con->onState([&](const TcpConnPtr& con) {
-            if (con->getState() == TcpConn::Connected) {
-                con->context<long>() = -1;
-            } else if (con->getState() == TcpConn::Closed) {
+            if (con->getState() == TcpConn::Closed) {
                 long id = con->context<long>();
                 if (id > 0) {
-                    users.erase(id);
+                    userConns.erase(id);
                 }
             }
         });
@@ -85,15 +144,15 @@ int main(int argc, const char* argv[]) {
             if (cm.type == ChatMsg::Login) {
                 exitif(id != -1 || cm.fromId <= 0, "login error");
                 id = cm.fromId;
-                users[id] = con;
+                userConns[id] = con;
                 info("user %ld login ok", id);
                 resp.data = "login ok";
             } else if (cm.type == ChatMsg::Chat) {
                 exitif(id < 0, "msg recved before login");
-                if (users.find(cm.toId) == users.end()) {
+                if (userConns.find(cm.toId) == userConns.end()) {
                     resp.data = "user not found";
                 } else {
-                    users[cm.toId]->sendMsg(cm.str());
+                    userConns[cm.toId]->sendMsg(cm.str());
                     return;
                 }
             } else if (cm.type == ChatMsg::Logout) {
